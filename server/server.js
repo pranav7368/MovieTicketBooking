f0762324dfdf06e597a0612ecfc3554f3619433e
Pranav Kumar Singh <pranav736891@gmail.com>
2025-11-15 23:16:10 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+require('dotenv').config();
+
+// routes
+const auth = require('./routes/auth');
+const cinema = require('./routes/cinema');
+const theater = require('./routes/theater');
+const movie = require('./routes/movie');
+const showtime = require('./routes/showtime');
+
+// -------------------------------
+// CONNECT TO MONGODB ATLAS
+// -------------------------------
+mongoose.set('strictQuery', false);
+
+mongoose
+  .connect(process.env.DATABASE, { autoIndex: true })
+  .then(() => console.log("MongoDB Atlas Connected Successfully!"))
+  .catch((err) => console.log("MongoDB Connection Error:", err));
+
+// -------------------------------
+// EXPRESS APP
+// -------------------------------
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: true, credentials: true }));
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+
+// -------------------------------
+// ROUTES
+// -------------------------------
+app.use('/auth', auth);
+app.use('/cinema', cinema);
+app.use('/theater', theater);
+app.use('/movie', movie);
+app.use('/showtime', showtime);
+
+// -------------------------------
+// START SERVER
+// -------------------------------
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
